d2c7b2179b994721373447b5bd38ce30608db531
achimcc <achim@parity.io>
2022-04-22 10:11:20 +0200

    basic substrate connect call

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  ScProvider,
+  WellKnownChain,
+} from "@polkadot/rpc-provider/substrate-connect";
+import { ApiPromise } from "@polkadot/api";
+
+const subscribe = async () => {
+  // Create the provider for a known chain
+  const provider = new ScProvider(WellKnownChain.rococo_v2_1);
+  // Stablish the connection (and catch possible errors)
+  await provider.connect();
+  // Create the PolkadotJS api instance
+  const api = await ApiPromise.create({ provider });
+  await api.rpc.chain.subscribeNewHeads((lastHeader) => {
+    console.log(`head: ${lastHeader.hash}`);
+  });
+  await api.disconnect();
+};
 
 function App() {
+  subscribe();
   return (
     <div className="App">
       <header className="App-header">
